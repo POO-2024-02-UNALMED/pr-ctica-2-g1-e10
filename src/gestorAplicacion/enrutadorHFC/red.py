088b4d9bd324ddbaebf8cbf0a38f4956d8867fb8
Maria José Monroy Mejía <mamonroym@unal.edu.co>
2025-02-12 21:57:56 -0500

    Update red.py

--- src/gestorAplicacion/enrutadorHFC/red.py
+++ src/gestorAplicacion/enrutadorHFC/red.py
@@ -1,3 +1,16 @@
-class Red():
+from abc import ABC,abstractmethod
 
+# CLASE ABSTRACTA
+class Red(ABC):
+  
+  # // "CONSTANTES"
   FLUJO_RED_PRELIMINAR=500
+
+  # //MÉTODOS
+  @abstractmethod
+  def verificarAdmin(self,proveedores,nombre):
+    pass
+
+  @abstractmethod
+  def distanciasOptimas(self,clientes,listaIntensidadesClientes):
+    pass
