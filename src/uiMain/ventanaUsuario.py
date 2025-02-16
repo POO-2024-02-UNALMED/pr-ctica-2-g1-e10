47bbe7d5987bc6419e68901c87191b2c65cdb721
vamoreno23 <152014036+vamoreno23@users.noreply.github.com>
2025-02-16 17:23:56 -0500

    ventanaUsuario

--- src/uiMain/ventanaUsuario.py
+++ src/uiMain/ventanaUsuario.py
@@ -59,11 +59,6 @@ class VentanaUsuario:
          def finalizar(): 
             programa()
 
-
-
-
-
-
             # //////////////////////////////////////////////////////////////////////////////////////////////////////
             # SE ELIMINAN LOS FRAMES YA CREADOS----PARA CAMBIAR ENTRE FUNCIONALIDADES
             for widget in frameInformacion.winfo_children():
@@ -87,3 +82,71 @@ class VentanaUsuario:
             borrar_button = tk.Button( frameInterior, text="Borrar", command=fp.borrarValores)
             aceptar_button.pack( side="left", pady=20, padx=50, anchor="sw")
             borrar_button.pack( side="left", pady=20, padx=40, anchor="se")
+
+
+
+
+
+
+
+
+
+
+ 
+         # FRAME PRINCIPAL
+         mainFrame = tk.Frame(ventana,bg="gray85", height=470,width=770,padx=10, pady=10)
+         mainFrame.pack(expand=True, fill="both", padx=10, pady=10)
+
+         # FRAME TITULO
+         frameTitulo = tk.Frame(mainFrame, bg="gray85", height=50, width=200)
+         frameTitulo.pack(fill="both",side="top", padx=2, pady=2)
+
+         # FRAME DESCRIPCIÓN
+         frameDescripcion = tk.Frame(mainFrame, bg="gray85", height=70, width=700)
+         frameDescripcion.pack(fill="both", padx=10, pady=10)
+
+         # # FRAME QUE CONTIENE EL FIELDFRAME
+         frameInformacion = tk.Frame(mainFrame, bg="gray82", height=260, width=670)
+         frameInformacion.pack(expand=True, fill="both", padx=5, pady=5)
+
+         # CREAR MENU
+         menuBar = tk.Menu(ventana)
+         ventana.config(menu=menuBar)
+
+         # MENU ARCHIVO
+         menuArchivo = tk.Menu(menuBar, tearoff = False)
+         menuBar.add_cascade(label="Archivo", menu=menuArchivo)
+         menuArchivo.add_command(label="Aplicación", command=eventoAplicacion)
+         menuArchivo.add_command(label="Salir", command=eventoSalir)
+
+         # MENSAJE INICIAL
+         self.titulo = tk.Label(frameTitulo,text="BIENVENIDO A LA VENTANA PRINCIPAL", font=("Times", 12), bg="gray85")
+         self.titulo.pack(fill="x")
+
+         self.descripcion = tk.Label(frameDescripcion, text=" A continuación te daremos las instrucciones sobre cómo utilizar la aplicación:\n\n1. En la parte superior de esta ventana encontrarás un menú con tres pestañas: Archivo, Procesos y Consultas, Ayuda.\n\n2. En Archivo se desplegarán dos opciones:\n\n\t2.1. Aplicación, te dará información sobre el programa.\n\t2.2. Salir, sirve para regresar a la ventana de inicio. \n\n3. En Procesos y Consultas se listarán cada una de las funcionalidades. Da click en la opción que desees. \n\n4. Finalmente, en Ayuda tendrás la opción de Acerca de, esto te dará información sobre los autores del programa.", font=("Times",12), bg="gray85", justify="left")
+         self.descripcion.pack(fill="x")
+         
+
+         # MENU FUNCIONALIDADES
+         menuFuncionalidades = tk.Menu(menuBar, tearoff = False)
+         menuBar.add_cascade(label="Procesos y Consultas",menu=menuFuncionalidades)
+         menuFuncionalidades.add_command(label="Adquisición de un Plan", command=adquisicionPlan)
+         menuFuncionalidades.add_command(label="Visualizar Dispositivos Conectados", command=visuaDispo)
+         menuFuncionalidades.add_command(label="Mejora tu Plan",command=mejoraTuPlan)
+         menuFuncionalidades.add_command(label="Test de Velocidad", command=test_command)
+         menuFuncionalidades.add_command(label="Reporte", command=reporte)
+
+         # MENU AYUDA
+         menuAyuda = tk.Menu(menuBar, tearoff = False)
+         menuAyuda.add_command(label="Acerca de",command=eventoAcercaDe)
+         menuBar.add_cascade(label="Ayuda", menu=menuAyuda)
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      # SE CREA LA VENTANA
+      ventana = tk.Tk()
+      ventana.title("Sistema de Redes")
+      ventana.geometry("800x500")
+      VentanaUsuario._abierto = True
+      programa()
+
